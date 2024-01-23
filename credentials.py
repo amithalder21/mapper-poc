3fdbceb303f5f58045e49d2d2e024e468e11b3ed
Amit Halder <36619176+amithalder21@users.noreply.github.com>
2024-01-23 15:16:18 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,33 @@
+import json
+import subprocess
+
+# Specify the path to the file containing JSON data
+file_path = 'tempcred.txt'
+
+# Read JSON data from the file
+with open(file_path, 'r') as file:
+    json_data = file.read()
+
+# Load JSON data
+data = json.loads(json_data)
+
+# Extract values
+access_key_id = data["Credentials"]["AccessKeyId"]
+secret_access_key = data["Credentials"]["SecretAccessKey"]
+session_token = data["Credentials"]["SessionToken"]
+
+# Use the AWS CLI to dynamically get the AWS account ID
+account_id = "$CLIENT_ACCOUNT_ID"
+#subprocess.check_output(['aws', 'sts', 'get-caller-identity', '--output', 'json']).decode('utf-8')
+#account_id = json.loads(account_id)['Account']
+
+# Configure AWS CLI with extracted values for the new profile
+subprocess.run(['aws', 'configure', '--profile', account_id, 'set', 'aws_access_key_id', access_key_id])
+subprocess.run(['aws', 'configure', '--profile', account_id, 'set', 'aws_secret_access_key', secret_access_key])
+subprocess.run(['aws', 'configure', '--profile', account_id, 'set', 'aws_session_token', session_token])
+
+# Set the default region for the new profile
+subprocess.run(['aws', 'configure', '--profile', account_id, 'set', 'region', 'ap-south-1'])
+
+# Optionally, set the default output format to JSON
+subprocess.run(['aws', 'configure', '--profile', account_id, 'set', 'output', 'json'])
